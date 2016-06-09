788af20a69f73621f7f94b737ac3714ee782bd1b
jbocharov <john@voicebase.com>
2016-06-09 15:06:43 -0700

    Add Mule Anypoint Studio to the tap

---
+++ anypoint-studio.rb
@@ -0,0 +1,10 @@
+ass AnypointStudio < Cask
+  version '6.0.0-GA'
+  sha256 ''
+
+  url 'https://mule-studio.s3.amazonaws.com/6.0.0-GA/AnypointStudio-for-macosx-64bit-6.0.0-201605161739.zip'
+  homepage 'https://www.mulesoft.com/ty/dl/studio-mac'
+  license :unknown
+
+  app ''
+end
